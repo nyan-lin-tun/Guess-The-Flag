96867b23cce79e98ebdf87fa88628f5c6eee84df
Nyan Lin Tun <nyanlynntunn@gmail.com>
2020-12-19 02:14:40 +0630

    Initial Commit

---
+++ GuessTheFlag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Nyan Lin Tun on 19/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Nyan Lin Tun on 19/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
